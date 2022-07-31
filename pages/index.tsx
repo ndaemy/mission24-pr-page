07a96ff61948542bf9e9623175bb5e3b0438e134
ndaemy <yuyaebean@gmail.com>
2022-07-31 23:55:55 +0900

    💄 Install chakra ui

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,12 @@
+import { Center, Heading } from "@chakra-ui/react";
 import type { NextPage } from "next";
 
 const Home: NextPage = () => {
-  return <div>Hello</div>;
+  return (
+    <Center bg="tomato" h="100px" color="white">
+      <Heading>Hello</Heading>
+    </Center>
+  );
 };
 
 export default Home;
