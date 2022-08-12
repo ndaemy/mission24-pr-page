fa9c5a7c03b2e8f0f77ae8cdf33de2311ebda619
ndaemy <yuyaebean@gmail.com>
2022-08-13 03:21:26 +0900

    :sparkles: Add logo to header bar

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,12 +1,13 @@
-import { Box, Button, Flex, Text } from "@chakra-ui/react";
+import { Button, Flex } from "@chakra-ui/react";
 import type { FC } from "react";
+import Logo from "~images/logo_w.svg";
 
 const Header: FC = () => {
   return (
     <Flex height="60px" px={["24px", "36px"]} justifyContent="center">
       <Flex justifyContent="space-between" alignItems="center" maxWidth="1140px" width="100%">
-        <Box>Logo</Box>
-        <Flex gap="24px">
+        <Logo height="16px" />
+        <Flex gap={["12px", "24px"]}>
           <Button variant="ghost">Start</Button>
           <Button variant="ghost">Information</Button>
           <Button variant="ghost">Ticket</Button>
