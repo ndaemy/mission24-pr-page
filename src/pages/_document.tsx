86ace83fbbcb150296e14afceeffe4bc0a91b490
ndaemy <yuyaebean@gmail.com>
2022-08-24 08:11:49 +0900

    🔍️ Apply og title, desc, image and page title

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -11,6 +11,12 @@ const Document = () => {
           type="text/javascript"
           src="//dapi.kakao.com/v2/maps/sdk.js?appkey=e3fb0ce1efdaa74039c9d98cde40856c&libraries=services,clusterer"
         />
+        <meta property="og:title" content="Mission24" />
+        <meta property="og:description" content="멋쟁이사자처럼 경기남부연합 해커톤" />
+        <meta property="og:image" content="/og.png" />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" />
+        <title>Mission24</title>
       </Head>
       <body>
         <ColorModeScript initialColorMode={theme.config.initialColorMode} />
